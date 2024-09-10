e9a910a8f8ecafb64dee312d3a8a5f14cb5760a2
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-09-10 11:21:28 +0200

    Refactor routing algorithm to use Ant Colony Optimization

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,15 +1,125 @@
 <template>
   <div id="map" style="height: 800px;"></div>
   <div>
-    <h1>Tri par voisin 2opt</h1>
-
-    <div>Total Distance: {{ totalDistance }} km</div>
+    <h1>Tri par la Colonie de Fourmis</h1>
+    <div>Distance Totale : {{ totalDistance }} km</div>
   </div>
 </template>
 
 <script>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+class AntColonyOptimization {
+  constructor(graph, numAnts, maxIterations, alpha, beta, evaporation) {
+    this.graph = graph;
+    this.numAnts = numAnts;
+    this.maxIterations = maxIterations;
+    this.alpha = alpha;
+    this.beta = beta;
+    this.evaporation = evaporation;
+    this.pheromone = this.initializePheromone(graph.length);
+  }
+
+  initializePheromone(size) {
+    const pheromone = [];
+    for (let i = 0; i < size; i++) {
+      pheromone[i] = new Array(size).fill(1.0);
+    }
+    return pheromone;
+  }
+
+  optimize() {
+    let bestTour = null;
+    let bestLength = Infinity;
+
+    for (let iteration = 0; iteration < this.maxIterations; iteration++) {
+      const ants = this.createAnts();
+      for (const ant of ants) {
+        this.walk(ant);
+        const tourLength = this.length(ant);
+        if (tourLength < bestLength) {
+          bestLength = tourLength;
+          bestTour = ant.slice();
+        }
+      }
+      this.updatePheromone(ants);
+    }
+
+    return { bestTour, bestLength };
+  }
+
+  createAnts() {
+    const ants = [];
+    for (let i = 0; i < this.numAnts; i++) {
+      ants.push(this.createAnt());
+    }
+    return ants;
+  }
+
+  createAnt() {
+    const tour = new Array(this.graph.length).fill(null);
+    tour[0] = Math.floor(Math.random() * this.graph.length);
+    for (let i = 1; i < this.graph.length; i++) {
+      tour[i] = this.selectNext(tour[i - 1], tour);
+    }
+    return tour;
+  }
+
+  selectNext(current, tour) {
+    const probabilities = [];
+    for (let i = 0; i < this.graph.length; i++) {
+      if (tour.includes(i)) {
+        probabilities.push(0);
+      } else {
+        const pheromone = this.pheromone[current][i] ** this.alpha;
+        const heuristic = (1.0 / this.graph[current][i]) ** this.beta;
+        probabilities.push(pheromone * heuristic);
+      }
+    }
+    const sum = probabilities.reduce((a, b) => a + b, 0);
+    probabilities.forEach((v, i) => probabilities[i] = v / sum);
+
+    let pSum = 0;
+    const rand = Math.random();
+    for (let i = 0; i < probabilities.length; i++) {
+      pSum += probabilities[i];
+      if (rand < pSum) {
+        return i;
+      }
+    }
+    return probabilities.length - 1;
+  }
+
+  walk(tour) {
+    for (let i = 1; i < tour.length; i++) {
+      tour[i] = this.selectNext(tour[i - 1], tour);
+    }
+  }
+
+  length(tour) {
+    let length = 0;
+    for (let i = 1; i < tour.length; i++) {
+      length += this.graph[tour[i - 1]][tour[i]];
+    }
+    length += this.graph[tour[tour.length - 1]][tour[0]];
+    return length;
+  }
+
+  updatePheromone(ants) {
+    this.pheromone.forEach((row, i) => row.forEach((value, j) => { this.pheromone[i][j] *= (1.0 - this.evaporation); }));
+
+    for (const ant of ants) {
+      const deltaPheromone = 1.0 / this.length(ant);
+      for (let i = 1; i < ant.length; i++) {
+        this.pheromone[ant[i - 1]][ant[i]] += deltaPheromone;
+        this.pheromone[ant[i]][ant[i - 1]] += deltaPheromone;
+      }
+      this.pheromone[ant[ant.length - 1]][ant[0]] += deltaPheromone;
+      this.pheromone[ant[0]][ant[ant.length - 1]] += deltaPheromone;
+    }
+  }
+}
 
 export default {
   name: 'MapComponent',
@@ -20,7 +130,6 @@ export default {
     };
   },
   methods: {
-    // Méthode pour calculer la distance haversine entre deux coordonnées GPS
     haversineDistance(coord1, coord2) {
       const toRad = angle => angle * (Math.PI / 180);
       const R = 6371; // Rayon de la Terre en kilomètres
@@ -37,62 +146,39 @@ export default {
 
       return R * c;
     },
-    // Générer le polyligne du chemin
     generatePath(map, path) {
-      path.forEach((segment) => {
-        const [start, end] = segment;
-        L.polyline([start, end], { color: 'blue' }).addTo(map);
-      });
+      const latlngs = path.map(coord => L.latLng(coord[0], coord[1]));
+      L.polyline(latlngs, { color: 'blue' }).addTo(map);
     },
-    // Calculer la distance totale
     calculateTotalDistance(path) {
       return path.reduce((total, point, index) => {
-        if (index === path.length - 1) return total; // Pas de retour au point de départ
+        if (index === path.length - 1) return total;
         return total + this.haversineDistance(point, path[index + 1]);
       }, 0);
     },
-    // Algorithme 2-opt pour optimiser le trajet
-    twoOpt(path) {
-      const swap = (path, i, k) => {
-        return path.slice(0, i)
-            .concat(path.slice(i, k + 1).reverse())
-            .concat(path.slice(k + 1));
-      };
-
-      const totalDistance = (path) => {
-        let dist = 0;
-        for (let i = 0; i < path.length - 1; i++) {
-          dist += this.haversineDistance(path[i], path[i + 1]);
-        }
-        return dist; // Pas de retour au point de départ
-      };
-
-      let improvement = true;
-      while (improvement) {
-        improvement = false;
-        for (let i = 1; i < path.length - 1; i++) {
-          for (let k = i + 1; k < path.length; k++) {
-            const newPath = swap(path, i, k);
-            if (totalDistance(newPath) < totalDistance(path)) {
-              path = newPath;
-              improvement = true;
-            }
-          }
+    antColonyOptimization(path) {
+      const graph = [];
+      for (let i = 0; i < path.length; i++) {
+        graph[i] = [];
+        for (let j = 0; j < path.length; j++) {
+          graph[i][j] = this.haversineDistance(path[i], path[j]);
         }
       }
-      return path;
+
+      const aco = new AntColonyOptimization(graph, 10, 100, 1.0, 5.0, 0.5);
+      const result = aco.optimize();
+      const optimizedPath = result.bestTour.map(i => path[i]);
+      this.totalDistance = result.bestLength;
+      return optimizedPath;
     }
   },
   mounted() {
-    // Initialisation de la carte
     const map = L.map('map');
 
-    // Ajout du layer OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributeurs'
     }).addTo(map);
 
-    // Coordonnées GPS des marqueurs
     const coordinates = [
       [45.171112, 5.695952],
       [45.183152, 5.699386],
@@ -120,49 +206,22 @@ export default {
       [45.171244, 5.689872]
     ];
 
-    // Ajout du marqueur pour le point de départ
-    L.marker(coordinates[0], {
-      icon: L.divIcon({
-        className: 'custom-div-icon',
-        html: `<img class="leaflet-marker-icon" src="https://leafletjs.com/examples/custom-icons/leaf-red.png" /><div class="number-marker" style="color: white;">0</div>`,
-        iconSize: [30, 42],
-        iconAnchor: [15, 42]
-      })
-    }).addTo(map).bindPopup("<b>Point de départ</b><br />Voici votre point de départ.").openPopup();
+    this.path = this.antColonyOptimization(coordinates);
+    this.totalDistance = this.calculateTotalDistance(this.path);
 
-    // Calcul des limites pour centrer la carte
-    const bounds = L.latLngBounds(coordinates);
-    map.fitBounds(bounds);
-
-    // Générer un chemin initial
-    let path = coordinates.slice(1); // Exclure le point de départ du chemin initial
-
-    // Appliquer l'algorithme 2-opt pour optimiser le chemin
-    path = this.twoOpt(path);
-
-    // Ajouter le point de départ au début du chemin
-    path.unshift(coordinates[0]);
-
-    // Mettre à jour la distance totale
-    this.totalDistance = this.calculateTotalDistance(path);
-
-    // Générer le polyligne sur la carte
-    path.forEach((point, i) => {
-      L.marker(point, {
+    this.path.forEach((coord, index) => {
+      L.marker(coord, {
         icon: L.divIcon({
-          className: 'custom-div-icon',
-          html: `<img class="leaflet-marker-icon" src="${i === 0 ? 'https://leafletjs.com/examples/custom-icons/leaf-red.png' : 'https://leafletjs.com/examples/custom-icons/leaf-green.png'}" /><div class="number-marker" style="color: white;">${i}</div>`,
-          iconSize: [30, 42],
-          iconAnchor: [15, 42]
+          className: 'custom-icon',
+          html: `<div class="number-marker" style="background-color: blue; color: white;">${index}</div>`,
+          iconSize: [25, 25]
         })
-      }).addTo(map);
-
-      if (i < path.length - 1) {
-        L.polyline([point, path[i + 1]], { color: 'blue' }).addTo(map);
-      }
+      }).addTo(map)
+          .bindPopup(`<b>Point ${index}</b><br>Lat: ${coord[0]}, Lng: ${coord[1]}`);
     });
 
-    this.path = path; // Enregistrer le chemin pour une utilisation future si nécessaire
+    this.generatePath(map, this.path);
+    map.fitBounds(this.path);
   }
 };
 </script>
